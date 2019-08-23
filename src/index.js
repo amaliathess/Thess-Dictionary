2035eeeec91427b0cff9a6bd4a4b769f88ba474c
amalia <amaliatoumpa@yahoo.gr>
2019-08-23 22:24:22 +0300

    react props

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 
-import React from 'react'; // import react library if you have an ext.js it asumes you want it from node_modules
+import React,{Component} from 'react'; // import react library if you have an ext.js it asumes you want it from node_modules
 import ReactDOM from 'react-dom'; // is to render something on the root
-
+import JSON from './db.json'
 //COMPONENTS
 import Header from './components/header.js'
 import Playground from './components/playground.js'
+import NewsList from './components/list.js'
 
 // this is a component the main component
-const App =()=>{
+class App extends Component {
   //return React.createElement('h1', {className:'title'}, React.createElement('div',{className:'www'}, 'lkjlk'))
-
+  state = {
+  	news:JSON
+  }
+render(){
+	console.log(this.state.news);
   return (
   	<div>
   		<Header/>
+  		<NewsList news={this.state.news} amy='me'/>
   	</div>
 
   	)
+  }
 }
 ReactDOM.render(<App/>,document.querySelector('#root'));
 
